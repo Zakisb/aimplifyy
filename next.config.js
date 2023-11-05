1a234832451ec54dfbab50fe41bcd09768c1e148
Zakaria Sichaib <zakisb97@gmail.com>
2023-11-05 18:02:42 +0100

    1111

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const withNextIntl = require("next-intl/plugin")("./i18n.js");
-module.exports = withNextIntl({});
+module.exports = withNextIntl({
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        __dirname: true,
+      };
+    }
+
+    return config;
+  },
+});
