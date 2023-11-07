c0e870b812fe7991ba9dfa49bc05e937f9022a0a
Zakaria Sichaib <zakisb97@gmail.com>
2023-11-07 09:21:11 +0100

    22

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -50,21 +50,6 @@ export default async function RootLayout({ children, params: { locale } }) {
   const messages = await getMessages(locale);
   unstable_setRequestLocale(locale);
 
-  const polyfills = [
-    "Intl",
-    "Intl.Locale",
-    "Intl.DateTimeFormat",
-    `Intl.DateTimeFormat.~locale.${locale}`,
-    `Intl.NumberFormat`,
-    `Intl.NumberFormat.~locale.${locale}`,
-    "Intl.PluralRules",
-    `Intl.PluralRules.~locale.${locale}`,
-    "Intl.RelativeTimeFormat",
-    `Intl.RelativeTimeFormat.~locale.${locale}`,
-    "Intl.ListFormat",
-    `Intl.ListFormat.~locale.${locale}`,
-  ];
-
   return (
     <html lang={locale}>
       <NextIntlClientProvider
@@ -76,13 +61,6 @@ export default async function RootLayout({ children, params: { locale } }) {
           {children}
         </body>
       </NextIntlClientProvider>
-      <Script
-        strategy="beforeInteractive"
-        src={
-          "https://polyfill.io/v3/polyfill.min.js?features=" +
-          polyfills.join(",")
-        }
-      />
     </html>
   );
 }
