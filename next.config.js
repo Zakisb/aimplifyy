0281abdbf7dc12e2dd1abb18d274397f889862d6
Zakaria Sichaib <zakisb97@gmail.com>
2023-11-07 19:14:07 +0100

    - Updated next.js config

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
-const withNextIntl = require("next-intl/plugin")();
-module.exports = withNextIntl({});
+const withNextIntl = require("next-intl/plugin")("./i18n.js");
+module.exports = withNextIntl({
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        __dirname: true,
+      };
+    }
+
+    return config;
+  },
+});
